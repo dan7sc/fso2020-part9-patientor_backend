27574065ce8baefaeed2f8b401cb39692230b0b8
dan7sc <su7cheng@gmail.com>
2020-06-15 20:19:06 -0300

    Add solution to exercise 9.13: Patientor backend, step6

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,69 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import crypto from 'crypto';
-import { NewPatientEntry } from './types';
+import { NewPatientEntry, Gender } from './types';
 
-export const toNewPatientEntry = (object: any | string): NewPatientEntry => {
+const isString = (text: any): text is string => {
+    return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: any): boolean => {
+    return Boolean(Date.parse(date));
+};
+
+const isGender = (param: any): param is Gender => {
+    return Object.values(Gender).includes(param);
+};
+
+const isSSN = (param: string): param is string => {
+    const [a, b]: string[] = param.split('-');
+    return !isNaN(Number(a)) && a.length === 6 && b.length === 4;
+};
+
+const parseName = (name: any): string => {
+    if (!name || !isString(name)) {
+        throw new Error('Incorrect or missing name:');
+    }
+    return name;
+};
+
+const parseDateOfBirth = (dateOfBirth: any): string => {
+    if (!dateOfBirth || !isString(dateOfBirth) || !isDate(dateOfBirth)) {
+        throw new Error('Incorrect or missing date');
+    }
+    return dateOfBirth;
+};
+
+const parseSSN = (ssn: any): string => {
+    if (!ssn || !isString(ssn) || !isSSN(ssn)) {
+        throw new Error('Incorrect or missing ssn');
+    }
+    return ssn;
+};
+
+const parseGender = (gender: any): string => {
+    if (!gender || !isString(gender) || !isGender(gender)) {
+        throw new Error('Incorrect or missing gender');
+    }
+    return gender;
+};
+
+const parseOccupation = (occupation: any): string => {
+    if (!occupation || !isString(occupation)) {
+        throw new Error('Incorrect or missing occupation');
+    }
+    return occupation;
+};
+
+export const toNewPatientEntry = (object: any | undefined): NewPatientEntry => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { name, dateOfBirth, ssn, gender, occupation } = object;
 
     const newEntry: NewPatientEntry = {
-        name,
-        dateOfBirth,
-        ssn,
-        gender,
-        occupation
+        name: parseName(name),
+        dateOfBirth: parseDateOfBirth(dateOfBirth),
+        ssn: parseSSN(ssn),
+        gender: parseGender(gender),
+        occupation: parseOccupation(occupation)
     };
 
     return newEntry;
